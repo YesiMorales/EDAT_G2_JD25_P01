0c00864bb298bb42d0b0eb30987560a9ed6ea1ae
Yesiii Morales <yesiiimor19@gmail.com>
2025-08-25 09:38:41 -0600

    version_02: Formulario de datos aleatorios

---
+++ Datos.aleatorios/Program.cs
@@ -0,0 +1,19 @@
+using Datos.Aleatorios;
+
+namespace Datos.aleatorios
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new frmDatosAleatorios());
+        }
+    }
+}
